74005496571b0b62a8343638c9d745ce530fe850
Hassan AlShammari <hassan@masafat.co>
2020-12-30 21:46:42 +0300

    Fragment's finish & first one

--- whalessfragment/app/src/main/java/site/thewhale/whalessfragment/MainActivity.java
+++ whalessfragment/app/src/main/java/site/thewhale/whalessfragment/MainActivity.java
@@ -1,8 +1,11 @@
 package site.thewhale.whalessfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager vp = findViewById(R.id.vp);
+        TabLayout tl = findViewById(R.id.tl);
+
+        tl.setupWithViewPager(vp);
+
+        FragAdapter fragAdapter = new FragAdapter(getSupportFragmentManager());
+        vp.setAdapter(fragAdapter);
+
+        tl.getTabAt(0).setIcon(R.drawable.ic_baseline_star_24);
+        tl.getTabAt(1).setIcon(R.drawable.ic_baseline_sports_kabaddi_24);
+        tl.getTabAt(2).setIcon(R.drawable.ic_baseline_directions_run_24);
     }
 }
---
+++ whalessfragment/app/src/main/java/site/thewhale/whalessfragment/fragments/FragThree.java
@@ -0,0 +1,25 @@
+package site.thewhale.whalessfragment.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import site.thewhale.whalessfragment.R;
+
+public class FragThree extends Fragment {
+
+    public FragThree() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_three, container, false);
+    }
+}
